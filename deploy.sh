ce6d241b382c796f602930e14e8873e12e888bfb
panoskap <petr.panoska@uzis.cz>
2019-09-16 19:03:51 +0200

    deploy script fix

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ git init
 git add -A
 git commit -m 'deploy'
 git remote add origin https://github.com/panda098/hackujstat2019.git
-git push origin HEAD:gh-pages
+git push -f origin HEAD:gh-pages
 cd -
