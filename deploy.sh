051eb192adb9ae8a967a2a9fb33af6d847df9f42
panoskap <petr.panoska@uzis.cz>
2019-09-16 19:00:26 +0200

    deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 set -e
-npm run build
+yarn run build
 cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:<panda098>/<hackujstat2019>.git master:gh-pages
+git remote add origin https://github.com/panda098/hackujstat2019.git
+git push origin HEAD:gh-pages
 cd -
